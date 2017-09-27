12ad905e4baf9576c95e31f85a2ccc8babdc0da2
Łukasz Pankowski <lukpank@o2.pl>
2017-09-28 00:16:51 +0200

    initial commit

---
+++ jsonlexer.go
@@ -0,0 +1,457 @@
+// Copyright 2017 Łukasz Pankowski <lukpank at o2 dot pl>. All rights
+// reserved.  This source code is licensed under the terms of the MIT
+// license. See LICENSE file for details.
+
+package jsonlexer
+
+import (
+	"bytes"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+	"unicode/utf8"
+)
+
+type Lexer struct {
+	r     io.Reader
+	b     []byte
+	start bool
+	buf   [4096]byte
+	sbuf  bytes.Buffer
+}
+
+func New(r io.Reader) *Lexer {
+	return &Lexer{r: r}
+}
+
+// Delim should be use for the following characters: "[]{}:"
+func (l *Lexer) Delim(expected byte) error {
+	b, err := l.nonSpaceByte()
+	if err != nil {
+		return err
+	}
+	l.b = l.b[1:]
+	if b != expected {
+		return fmt.Errorf("expected %q but found %q", expected, b)
+	}
+	if expected == '[' || expected == '{' {
+		l.start = true
+	}
+	return nil
+}
+
+// More is used to check (and removing) of a comma (unless just after
+// '{' or '['). Should not be called twice (one by one).
+func (l *Lexer) More() (bool, error) {
+	b, err := l.nonSpaceByte()
+	if err != nil {
+		return false, err
+	}
+	if b == ']' || b == '}' {
+		return false, nil
+	}
+	if l.start {
+		l.start = false
+		return true, nil
+	}
+	if b != ',' {
+		return false, fmt.Errorf("expected ',' but found %q", b)
+	}
+	l.b = l.b[1:]
+	return true, nil
+}
+
+func (l *Lexer) EOF() error {
+	b, err := l.nonSpaceByte()
+	if err != nil {
+		if err == io.EOF {
+			return nil
+		}
+		return err
+	}
+	return fmt.Errorf("expected EOF but found %q", b)
+}
+
+func (l *Lexer) Int64() (int64, error) {
+	_, err := l.nonSpaceByte()
+	if err != nil {
+		return 0, err
+	}
+	j := -1
+	for {
+		for i, c := range l.b {
+			if c == '-' && i == 0 {
+				continue
+			}
+			if c < '0' || c > '9' {
+				j = i
+				break
+			}
+		}
+		if j != -1 {
+			break
+		}
+		if len(l.b) == len(l.buf) {
+			return 0, errors.New("int64 number to long")
+		}
+		n := copy(l.buf[:], l.b)
+		m, err := l.r.Read(l.buf[n:])
+		if err != nil {
+			if err == io.EOF {
+				l.b = l.buf[:n+m]
+				j = n + m
+				break
+			}
+			return 0, err
+		}
+		l.b = l.buf[:n+m]
+	}
+	s := string(l.b[:j])
+	l.b = l.b[j:]
+	return strconv.ParseInt(s, 10, 64)
+}
+
+func (l *Lexer) Float64() (float64, error) {
+	_, err := l.nonSpaceByte()
+	if err != nil {
+		return 0, err
+	}
+	j := -1
+	for {
+		for i, c := range l.b {
+			if (c < '0' || c > '9') && c != '-' && c != '+' && c != '.' && c != 'e' && c != 'E' {
+				j = i
+				break
+			}
+		}
+		if j != -1 {
+			break
+		}
+		if len(l.b) == len(l.buf) {
+			return 0, errors.New("float64 number to long")
+		}
+		n := copy(l.buf[:], l.b)
+		m, err := l.r.Read(l.buf[n:])
+		if err != nil {
+			if err == io.EOF {
+				l.b = l.buf[:n+m]
+				j = n + m
+				break
+			}
+			return 0, err
+		}
+		l.b = l.buf[:n+m]
+	}
+	s := string(l.b[:j])
+	l.b = l.b[j:]
+	return strconv.ParseFloat(s, 64)
+}
+
+func (l *Lexer) Bool() (bool, error) {
+	b, err := l.nonSpaceByte()
+	if err != nil {
+		return false, err
+	}
+	var s string
+	var v bool
+	if b == 'f' {
+		s = "false"
+		v = false
+	} else if b == 't' {
+		s = "true"
+		v = true
+	} else {
+		return false, errors.New(`expected true or false`)
+	}
+	for {
+		for i, c := range l.b {
+			if i == len(s) {
+				l.b = l.b[i:]
+				return v, nil
+			}
+			if c != s[i] {
+				return false, errors.New(`expected true or false`)
+			}
+		}
+		if len(l.b) >= len(s) {
+			l.b = l.b[len(s):]
+			return v, nil
+		}
+		if len(l.b) == len(l.buf) {
+			return false, errors.New("bool value to long")
+		}
+		n := copy(l.buf[:], l.b)
+		m, err := l.r.Read(l.buf[n:])
+		if err != nil {
+			if err == io.EOF {
+				return false, io.ErrUnexpectedEOF
+			}
+			return false, err
+		}
+		l.b = l.buf[:n+m]
+	}
+}
+
+func (l *Lexer) String() (string, error) {
+	b, err := l.nonSpaceByte()
+	if err != nil {
+		return "", err
+	}
+	if b != '"' {
+		return "", errors.New(`expected '"' to start string`)
+	}
+	j := len(l.b)
+	for i, c := range l.b[1:] {
+		if c == '\\' || c >= 0x80 {
+			j = i + 1
+			break
+		}
+		if c == '"' {
+			j := i + 1
+			s := string(l.b[1:j])
+			l.b = l.b[j+1:]
+			return s, nil
+		}
+	}
+	l.sbuf.Reset()
+	l.sbuf.Write(l.b[1:j])
+	l.b = l.b[j:]
+
+	escape := false
+	for {
+		k, err := l.complexStr(&escape)
+		if err != nil {
+			return "", err
+		}
+		if k != -1 {
+			s := l.sbuf.String()
+			l.sbuf.Reset()
+			l.b = l.b[k+1:]
+			return s, nil
+		}
+
+		n := copy(l.buf[:], l.b)
+		m, err := l.r.Read(l.buf[n:])
+		if err != nil {
+			if err == io.EOF {
+				return "", errors.New(`expected '"' ending but EOF encountered`)
+			}
+			return "", err
+		}
+		l.b = l.buf[:n+m]
+	}
+}
+
+func (l *Lexer) StringValue(expected string) error {
+	b, err := l.nonSpaceByte()
+	if err != nil {
+		return err
+	}
+	if b != '"' {
+		return errors.New(`expected '"' to start string`)
+	}
+	j := len(l.b)
+	for i, c := range l.b[1:] {
+		if c == '\\' || c >= 0x80 {
+			j = i + 1
+			break
+		}
+		if c == '"' {
+			j := i + 1
+			err := equal(l.b[1:j], expected)
+			l.b = l.b[j+1:]
+			return err
+		}
+	}
+	l.sbuf.Reset()
+	l.sbuf.Write(l.b[1:j])
+	l.b = l.b[j:]
+
+	escape := false
+	for {
+		k, err := l.complexStr(&escape)
+		if err != nil {
+			return err
+		}
+		if k != -1 {
+			err := equal(l.sbuf.Bytes(), expected)
+			l.sbuf.Reset()
+			l.b = l.b[k+1:]
+			return err
+		}
+
+		n := copy(l.buf[:], l.b)
+		m, err := l.r.Read(l.buf[n:])
+		if err != nil {
+			if err == io.EOF {
+				return errors.New(`expected '"' ending but EOF encountered`)
+			}
+			return err
+		}
+		l.b = l.buf[:n+m]
+	}
+}
+
+func equal(b []byte, s string) error {
+	if len(b) != len(s) {
+		return fmt.Errorf("expected string %q but got %q", s, b)
+	}
+	for i, c := range b {
+		if c != s[i] {
+			return fmt.Errorf("expected string %q but got %q", s, b)
+		}
+	}
+	return nil
+}
+
+func (l *Lexer) complexStr(escape *bool) (int, error) {
+	var x [2]byte
+	i := 0
+	for i < len(l.b) {
+		c := l.b[i]
+		if *escape {
+			switch c {
+			default:
+				return 0, fmt.Errorf("unexpected escaped char %q", c)
+			case '"', '\\', '/':
+				l.sbuf.WriteByte(c)
+			case 'b':
+				l.sbuf.WriteByte('\b')
+			case 'f':
+				l.sbuf.WriteByte('\f')
+			case 'n':
+				l.sbuf.WriteByte('\n')
+			case 'r':
+				l.sbuf.WriteByte('\r')
+			case 't':
+				l.sbuf.WriteByte('\t')
+			case 'u':
+				if i+5 > len(l.b) {
+					l.b = l.b[i:]
+					return -1, nil
+				}
+				if _, err := hex.Decode(x[:], l.b[i+1:i+5]); err != nil {
+					return 0, err
+				}
+				l.sbuf.WriteRune(rune(x[0])<<8 + rune(x[1]))
+				i += 4
+
+			}
+			*escape = false
+			i++
+			continue
+		}
+		if c == '\\' {
+			*escape = true
+			i++
+			continue
+		}
+		if c == '"' {
+			return i, nil
+		}
+		if c >= 0x80 {
+			if !utf8.FullRune(l.b[i:]) {
+				l.b = l.b[i:]
+				return -1, nil
+			}
+			r, n := utf8.DecodeRune(l.b[i:])
+			l.sbuf.WriteRune(r)
+			i += n
+			continue
+		}
+		l.sbuf.WriteByte(c)
+		i++
+	}
+	l.b = nil
+	return -1, nil
+}
+
+func (l *Lexer) Skip() error {
+	b, err := l.nonSpaceByte()
+	if err != nil {
+		return err
+	}
+	switch b {
+	default:
+		// TODO: support null
+		return fmt.Errorf("unexpected byte %q", b)
+	case '[':
+		return l.skipArray()
+	case '{':
+		return l.skipDict()
+	case '"':
+		_, err := l.String()
+		return err
+	case '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		_, err := l.Float64()
+		return err
+	case 't', 'f':
+		_, err := l.Bool()
+		return err
+	}
+}
+
+func (l *Lexer) skipArray() error {
+	if err := l.Delim('['); err != nil {
+		return err
+	}
+	for {
+		more, err := l.More()
+		if err != nil {
+			return err
+		}
+		if !more {
+			break
+		}
+		if err := l.Skip(); err != nil {
+			return err
+		}
+	}
+	return l.Delim(']')
+}
+
+func (l *Lexer) skipDict() error {
+	if err := l.Delim('{'); err != nil {
+		return err
+	}
+	for {
+		more, err := l.More()
+		if err != nil {
+			return err
+		}
+		if !more {
+			break
+		}
+		if err := l.Skip(); err != nil {
+			return err
+		}
+		if err := l.Delim(':'); err != nil {
+			return err
+		}
+		if err := l.Skip(); err != nil {
+			return err
+		}
+	}
+	return l.Delim('}')
+}
+
+func (l *Lexer) nonSpaceByte() (byte, error) {
+	for {
+		if len(l.b) == 0 {
+			n, err := l.r.Read(l.buf[:])
+			if err != nil {
+				return 0, err
+			}
+			l.b = l.buf[:n]
+		}
+		for len(l.b) > 0 {
+			b := l.b[0]
+			if b != ' ' && b != '\t' && b != '\r' && b != '\n' {
+				return b, nil
+			}
+			l.b = l.b[1:]
+		}
+	}
+}
---
+++ jsonlexer_test.go
@@ -0,0 +1,367 @@
+// Copyright 2017 Łukasz Pankowski <lukpank at o2 dot pl>. All rights
+// reserved.  This source code is licensed under the terms of the MIT
+// license. See LICENSE file for details.
+
+package jsonlexer_test
+
+import (
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/lukpank/jsonlexer"
+)
+
+func TestLexerEmptyArray(t *testing.T) {
+	r := &readers{S: " [\r\n\t]"}
+	for i := 0; i < r.Len(); i++ {
+		l := jsonlexer.New(r.Get(i))
+		if err := l.Delim('['); err != nil {
+			t.Fatalf("expected '[' but got error: %v", err)
+		}
+		if err := l.Delim(']'); err != nil {
+			t.Fatalf("expected ']' but got error: %v", err)
+		}
+	}
+}
+
+func TestLexerEmptyArrayMore(t *testing.T) {
+	for _, s := range []string{" [\r\n\t]", " [\r\n\t]\r\n"} {
+		r := &readers{S: s}
+		for i := 0; i < r.Len(); i++ {
+			l := jsonlexer.New(r.Get(i))
+			if err := l.Delim('['); err != nil {
+				t.Fatalf("expected '[' but got error: %v", err)
+			}
+			more, err := l.More()
+			if err != nil {
+				t.Fatalf("expected ',' but got error: %v", err)
+			}
+			if more {
+				t.Fatal("expected false but got true")
+			}
+			if err := l.Delim(']'); err != nil {
+				t.Fatalf("expected ']' but got error: %v", err)
+			}
+			if err := l.EOF(); err != nil {
+				t.Fatalf("expected EOF but got error: %v", err)
+			}
+		}
+	}
+}
+
+func TestLexerInt64(t *testing.T) {
+	for i, s := range []string{" \r\n-123", " \r\n-123 "} {
+		r := &readers{S: s}
+		for j := 0; j < r.Len(); j++ {
+			t.Run(fmt.Sprintf("s%d/%d", i, j), func(t *testing.T) {
+				l := jsonlexer.New(r.Get(j))
+				expectedInt64(t, l, -123)
+			})
+		}
+	}
+}
+
+func TestLexerArrayInt64(t *testing.T) {
+	r := &readers{S: " [\r\n123, -84\t]"}
+	for i := 0; i < r.Len(); i++ {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			l := jsonlexer.New(r.Get(i))
+			if err := l.Delim('['); err != nil {
+				t.Fatalf("expected '[' but got error: %v", err)
+			}
+			expectedMore(t, l)
+			expectedInt64(t, l, 123)
+			expectedMore(t, l)
+			expectedInt64(t, l, -84)
+			if err := l.Delim(']'); err != nil {
+				t.Fatalf("expected ']' but got error: %v", err)
+			}
+		})
+	}
+}
+
+func TestLexerFloat64(t *testing.T) {
+	for i, s := range []string{" \r\n-1.5", " \r\n-1.5 "} {
+		r := &readers{S: s}
+		for j := 0; j < r.Len(); j++ {
+			t.Run(fmt.Sprintf("s%d/%d", i, j), func(t *testing.T) {
+				l := jsonlexer.New(r.Get(j))
+				expectedFloat64(t, l, -1.5)
+			})
+		}
+	}
+}
+
+func TestLexerArrayFloat64(t *testing.T) {
+	r := &readers{S: " [\r\n1e3, -3.25e2\t]"}
+	for i := 0; i < r.Len(); i++ {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			l := jsonlexer.New(r.Get(i))
+			if err := l.Delim('['); err != nil {
+				t.Fatalf("expected '[' but got error: %v", err)
+			}
+			expectedMore(t, l)
+			expectedFloat64(t, l, 1000)
+			expectedMore(t, l)
+			expectedFloat64(t, l, -325)
+			if err := l.Delim(']'); err != nil {
+				t.Fatalf("expected ']' but got error: %v", err)
+			}
+		})
+	}
+}
+
+func TestLexerBool(t *testing.T) {
+	for i, s := range []string{" \r\ntrue", " \r\ntrue ", " \r\nfalse", " \r\nfalse "} {
+		r := &readers{S: s}
+		for j := 0; j < r.Len(); j++ {
+			t.Run(fmt.Sprintf("s%d/%d", i, j), func(t *testing.T) {
+				l := jsonlexer.New(r.Get(j))
+				expectedBool(t, l, i < 2)
+			})
+		}
+	}
+}
+
+func TestLexerArrayBool(t *testing.T) {
+	r := &readers{S: " [\r\ntrue, false\t]"}
+	for i := 0; i < r.Len(); i++ {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			l := jsonlexer.New(r.Get(i))
+			if err := l.Delim('['); err != nil {
+				t.Fatalf("expected '[' but got error: %v", err)
+			}
+			expectedMore(t, l)
+			expectedBool(t, l, true)
+			expectedMore(t, l)
+			expectedBool(t, l, false)
+			if err := l.Delim(']'); err != nil {
+				t.Fatalf("expected ']' but got error: %v", err)
+			}
+		})
+	}
+}
+
+func TestLexerString(t *testing.T) {
+	cases := []struct{ input, output string }{
+		{" \r\n\"test\"", "test"},
+		{" \r\n\"test\" ", "test"},
+		{`  "test\b\u0105ę\f\n\r\t"`, "test\bąę\f\n\r\t"},
+		{` "test\b\u0105ę\f\n\r\t" `, "test\bąę\f\n\r\t"},
+		{`  "test\b\u0105` + "\x80" + `ę\f\n\r\t"`, "test\bą\uFFFDę\f\n\r\t"},
+		{` "test\b\u0105ę` + "\x80" + `\f\n\r\t" `, "test\bąę\uFFFD\f\n\r\t"},
+	}
+	for i, c := range cases {
+		r := &readers{S: c.input}
+		for j := 0; j < r.Len(); j++ {
+			t.Run(fmt.Sprintf("s%d/%s", i, r.Name(j)), func(t *testing.T) {
+				l := jsonlexer.New(r.Get(j))
+				expectedString(t, l, c.output)
+			})
+		}
+	}
+}
+
+func TestLexerArrayString(t *testing.T) {
+	r := &readers{S: " [\r\n\"test\", \"123\"\t]"}
+	for i := 0; i < r.Len(); i++ {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			l := jsonlexer.New(r.Get(i))
+			if err := l.Delim('['); err != nil {
+				t.Fatalf("expected '[' but got error: %v", err)
+			}
+			expectedMore(t, l)
+			expectedString(t, l, "test")
+			expectedMore(t, l)
+			expectedString(t, l, "123")
+			if err := l.Delim(']'); err != nil {
+				t.Fatalf("expected ']' but got error: %v", err)
+			}
+		})
+	}
+}
+
+func TestLexerStringValue(t *testing.T) {
+	cases := []struct{ input, output string }{
+		{" \r\n\"test\"", "test"},
+		{" \r\n\"test\" ", "test"},
+		{`  "test\b\u0105ę\f\n\r\t"`, "test\bąę\f\n\r\t"},
+		{` "test\b\u0105ę\f\n\r\t" `, "test\bąę\f\n\r\t"},
+		{`  "test\b\u0105` + "\x80" + `ę\f\n\r\t"`, "test\bą\uFFFDę\f\n\r\t"},
+		{` "test\b\u0105ę` + "\x80" + `\f\n\r\t" `, "test\bąę\uFFFD\f\n\r\t"},
+	}
+	for i, c := range cases {
+		r := &readers{S: c.input}
+		for j := 0; j < r.Len(); j++ {
+			t.Run(fmt.Sprintf("s%d/%s", i, r.Name(j)), func(t *testing.T) {
+				l := jsonlexer.New(r.Get(j))
+				expectedStringValue(t, l, c.output)
+				l = jsonlexer.New(r.Get(j))
+				err := l.StringValue(c.output[1:])
+				if err == nil {
+					t.Fatal("expected error")
+				}
+			})
+		}
+	}
+}
+
+func expectedMore(t *testing.T, l *jsonlexer.Lexer) {
+	more, err := l.More()
+	if err != nil {
+		t.Fatalf("expected more but got error: %v", err)
+	}
+	if !more {
+		t.Fatal("expected more but got false")
+	}
+}
+
+func expectedInt64(t *testing.T, l *jsonlexer.Lexer, expected int64) {
+	got, err := l.Int64()
+	if err != nil {
+		t.Fatalf("expected %d but got error: %v", expected, err)
+	}
+	if got != expected {
+		t.Errorf("expected %d but got: %d", expected, got)
+	}
+}
+
+func expectedFloat64(t *testing.T, l *jsonlexer.Lexer, expected float64) {
+	got, err := l.Float64()
+	if err != nil {
+		t.Fatalf("expected %v but got error: %v", expected, err)
+	}
+	if got != expected {
+		t.Errorf("expected %v but got: %d", expected, got)
+	}
+}
+
+func expectedBool(t *testing.T, l *jsonlexer.Lexer, expected bool) {
+	got, err := l.Bool()
+	if err != nil {
+		t.Fatalf("expected %t but got error: %v", expected, err)
+	}
+	if got != expected {
+		t.Errorf("expected %t but got: %t", expected, got)
+	}
+}
+
+func expectedString(t *testing.T, l *jsonlexer.Lexer, expected string) {
+	got, err := l.String()
+	if err != nil {
+		t.Fatalf("expected %q but got error: %v", expected, err)
+	}
+	if got != expected {
+		t.Errorf("expected %q but got: %q", expected, got)
+	}
+}
+
+func expectedStringValue(t *testing.T, l *jsonlexer.Lexer, expected string) {
+	err := l.StringValue(expected)
+	if err != nil {
+		t.Fatalf("expected %q but got error: %v", expected, err)
+	}
+}
+
+func TestSplit1StringsReader(t *testing.T) {
+	r := &split1StringReader{s: "test1", split: 2}
+	b := make([]byte, 10)
+	n, err := r.Read(b)
+	if err != nil || n != 2 || string(b[:n]) != "te" {
+		t.Fatalf(`expected "te" but got %q (%v, %q)`, b[:n], n, err)
+	}
+	n, err = r.Read(b)
+	if err != nil || n != 3 || string(b[:n]) != "st1" {
+		t.Fatalf(`expected "st1" but got %q (%d, %v)`, b[:n], n, err)
+	}
+	n, err = r.Read(b)
+	if err != io.EOF || n != 0 {
+		t.Fatalf(`expected (0, io.EOF) but got %q (%d, %v)`, b[:n], n, err)
+	}
+}
+
+func TestSplitNStringsReader(t *testing.T) {
+	r := &splitNStringReader{s: "test1", split: 2}
+	b := make([]byte, 10)
+	n, err := r.Read(b)
+	if err != nil || n != 2 || string(b[:n]) != "te" {
+		t.Fatalf(`expected "te" but got %q (%v, %q)`, b[:n], n, err)
+	}
+	n, err = r.Read(b)
+	if err != nil || n != 2 || string(b[:n]) != "st" {
+		t.Fatalf(`expected "st1" but got %q (%d, %v)`, b[:n], n, err)
+	}
+	n, err = r.Read(b)
+	if err != nil || n != 1 || string(b[:n]) != "1" {
+		t.Fatalf(`expected "st1" but got %q (%d, %v)`, b[:n], n, err)
+	}
+	n, err = r.Read(b)
+	if err != io.EOF || n != 0 {
+		t.Fatalf(`expected (0, io.EOF) but got %q (%d, %v)`, b[:n], n, err)
+	}
+}
+
+type readers struct {
+	S string
+	i int
+}
+
+func (r *readers) Len() int {
+	return (3*len(r.S) - 1) / 2
+}
+
+func (r *readers) Get(i int) io.Reader {
+	if i < len(r.S) {
+		return &split1StringReader{s: r.S, split: i + 1}
+	}
+	i -= len(r.S) - 1
+	return &splitNStringReader{s: r.S, split: i}
+}
+
+func (r *readers) Name(i int) string {
+	if i < len(r.S) {
+		return fmt.Sprintf("1_%d", i+1)
+	}
+	i -= len(r.S) - 1
+	return fmt.Sprintf("N_%d", i)
+}
+
+type split1StringReader struct {
+	s     string
+	split int
+	pos   int
+}
+
+func (r *split1StringReader) Read(b []byte) (int, error) {
+	if r.pos == len(r.s) {
+		return 0, io.EOF
+	}
+	if r.pos < r.split {
+		n := copy(b, r.s[r.pos:r.split])
+		r.pos += n
+		return n, nil
+	}
+	n := copy(b, r.s[r.pos:])
+	r.pos += n
+	return n, nil
+}
+
+type splitNStringReader struct {
+	s     string
+	split int
+	pos   int
+}
+
+func (r *splitNStringReader) Read(b []byte) (int, error) {
+	if r.pos == len(r.s) {
+		return 0, io.EOF
+	}
+	end := r.pos + r.split
+	if end > len(r.s) {
+		end = len(r.s)
+	}
+	n := copy(b, r.s[r.pos:end])
+	r.pos += n
+	return n, nil
+}
